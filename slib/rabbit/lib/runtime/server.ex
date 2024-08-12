d1c26428c62f20322aa5dec9bfad4d130a7d8bda
Anil Kulkarni <octane222@users.noreply.github.com>
2024-08-12 19:56:10 -0400

    refactored server

--- slib/rabbit/lib/runtime/server.ex
+++ slib/rabbit/lib/runtime/server.ex
@@ -30,31 +30,28 @@ defmodule Rabbit.Runtime.Server do
   # Client
   # ========================================================================
 
+  @spec start_link(Config.t()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_link(%Config{} = config) do
     GenServer.start_link(__MODULE__, config, name: via_tuple(config.id))
   end
 
   # ------------------------------------------------------------------------
 
-  defp via_tuple(id) do
-    name = "rabbit_server_#{id}"
-    {:via, Registry, {Rabbit.Registry, name}}
-  end
-
-  # ------------------------------------------------------------------------
-
+  @spec get_state(non_neg_integer()) :: Server.t()
   def get_state(server_id) do
     GenServer.call(via_tuple(server_id), :get_state)
   end
 
   # ------------------------------------------------------------------------
 
+  @spec refresh(non_neg_integer()) :: Server.t()
   def refresh(server_id) do
-    GenServer.call(via_tuple(server_id), :refresh_server)
+    GenServer.call(via_tuple(server_id), :refresh)
   end
 
   # ------------------------------------------------------------------------
 
+  @spec purge_queue(non_neg_integer(), String.t()) :: :ok | {:error, String.t()}
   def purge_queue(server_id, queue_name) do
     GenServer.call(via_tuple(server_id), {:purge_queue, queue_name})
   end
@@ -88,7 +85,7 @@ defmodule Rabbit.Runtime.Server do
   end
 
   @impl true
-  def handle_call(:refresh_server, _from, %Server{} = state) do
+  def handle_call(:refresh, _from, %Server{} = state) do
     new_state = refresh_server(state)
     {:reply, new_state, new_state}
   end
@@ -103,6 +100,13 @@ defmodule Rabbit.Runtime.Server do
 
   # ------------------------------------------------------------------------
 
+  defp via_tuple(id) do
+    name = "rabbit_server_#{id}"
+    {:via, Registry, {Rabbit.Registry, name}}
+  end
+
+  # ------------------------------------------------------------------------
+
   defp refresh_server(%Server{} = state) do
     with {:ok, queues} <- Manager.get_queues(state.config),
          {:ok, %{"status" => "ok"}} <- Manager.check_health(state.config) do
@@ -110,7 +114,7 @@ defmodule Rabbit.Runtime.Server do
     else
       {:error, msg} ->
         Logger.error("Error: #{inspect(msg)}")
-        %{state | state: {:error, msg}}
+        %{state | state: {:error, msg}, health: "Unknown"}
     end
   end
 end
